6ff2858c06ded84b3ce98b69dbcad5d823455515
Faradayyg <faradayyg@gmail.com>
2017-11-04 07:26:21 +0100

    edit procfile

--- serve.js
+++ serve.js
@@ -17,6 +17,6 @@ app.get('*',function(req,res){
 	res.sendFile(path.resolve(__dirname,'./views/index.html'));
 });
 
-app.listen(3000,function(){
-	console.log('Running on :3000...');
+app.listen(39072,function(){
+	console.log('Running on :39072...');
 });
